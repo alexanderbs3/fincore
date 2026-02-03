25d7ed23ac6cf69c4d16c51606da9e23461e959c
Alexander Costa <alexander.cbss@hotmail.com>
2026-02-03 11:06:30 -0300

    configuração inicial do ambiente com docker-compose, .env e datasource

---
+++ src/test/java/br/leetjourney/fincore/FincoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.leetjourney.fincore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FincoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
