ec153c9aadbfb4e109cfb0775ab250daca8e99a3
Alexander Costa <alexander.cbss@hotmail.com>
2026-02-03 14:19:54 -0300

    Adicionando as estruturas entitys

---
+++ src/main/java/br/leetjourney/fincore/core/entity/Customer.java
@@ -0,0 +1,32 @@
+package br.leetjourney.fincore.core.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer extends BaseEntity {
+
+    @Column(name = "fullName", nullable = false, length = 150)
+    private String fullName;
+
+    @Column(name = "document_number",nullable = false,unique = true,length = 14)
+    private String documentNumber;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false,length = 20)
+    private CustomerStatus status;
+
+
+
+
+}
