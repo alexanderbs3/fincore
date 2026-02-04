5cb4cb112feec9820a757abdb54e47468a285b49
Alexander Costa <alexander.cbss@hotmail.com>
2026-02-04 10:09:01 -0300

    Implementacao do security, entity user, enums e repository.

---
+++ src/main/java/br/leetjourney/fincore/core/security/TokenService.java
@@ -0,0 +1,46 @@
+package br.leetjourney.fincore.core.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public  String generateToken(User user) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.create()
+                .withIssuer("fincore-api")
+                .withSubject(user.getEmail())
+                .withExpiresAt(genExpirationDate())
+                .sign(algorithm);
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("fincore-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    private Instant genExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
