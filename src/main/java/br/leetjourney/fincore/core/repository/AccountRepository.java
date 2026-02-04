43d4fe271b4d48e9cd25c4e879ff3775a00abce8
Alexander Costa <alexander.cbss@hotmail.com>
2026-02-04 14:41:50 -0300

    criancao do repository account

---
+++ src/main/java/br/leetjourney/fincore/core/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package br.leetjourney.fincore.core.repository;
+
+import br.leetjourney.fincore.core.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
+    Optional<Account> findByUuid(String uuid);
+    boolean existsByAccountNumber(String accountNumber);
+    List<Account> findAllByCustomerUuid(String customerUuid);
+}
