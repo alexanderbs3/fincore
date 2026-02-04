63f978323dd9efedb02d4104efa296d6abd12298
Alexander Costa <alexander.cbss@hotmail.com>
2026-02-04 13:05:06 -0300

    criacao do UserRepository

--- src/main/java/br/leetjourney/fincore/core/repository/UserRepository.java
+++ src/main/java/br/leetjourney/fincore/core/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package br.leetjourney.fincore.core.repository;
 
+import br.leetjourney.fincore.api.dto.RegisterRequest;
 import br.leetjourney.fincore.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
+
+    // Alternativa sênior: existsBy para verificações rápidas de boolean
+    boolean existsByEmail(String email);
 }
