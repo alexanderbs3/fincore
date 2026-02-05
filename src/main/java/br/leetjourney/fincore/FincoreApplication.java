a27d65c5700ad8aa630e2c4a8eba62cd411ca82c
Alexander Costa <alexander.cbss@hotmail.com>
2026-02-05 20:03:31 -0300

    atualizacao da classe main

--- src/main/java/br/leetjourney/fincore/FincoreApplication.java
+++ src/main/java/br/leetjourney/fincore/FincoreApplication.java
@@ -2,8 +2,10 @@ package br.leetjourney.fincore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FincoreApplication {
 
     public static void main(String[] args) {
