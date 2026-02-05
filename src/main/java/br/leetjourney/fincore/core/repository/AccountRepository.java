19cc98e3d1ce8b10018018c918b5abb05033440e
Alexander Costa <alexander.cbss@hotmail.com>
2026-02-05 20:02:11 -0300

    Atualizacao AccountRepository

--- src/main/java/br/leetjourney/fincore/core/repository/AccountRepository.java
+++ src/main/java/br/leetjourney/fincore/core/repository/AccountRepository.java
@@ -1,15 +1,22 @@
 package br.leetjourney.fincore.core.repository;
 
 import br.leetjourney.fincore.core.entity.Account;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
     Optional<Account> findByUuid(String uuid);
-    boolean existsByAccountNumber(String accountNumber);
-    List<Account> findAllByCustomerUuid(String customerUuid);
 
-    boolean existsByUuid(String accountUuid);
+    // O SELECT ... FOR UPDATE bloqueia a linha no MySQL até o commit/rollback
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT a FROM Account a WHERE a.uuid = :uuid")
+    Optional<Account> findByUuidWithLock(@Param("uuid") String uuid);
+
+    boolean existsByUuid(String uuid);
 }
