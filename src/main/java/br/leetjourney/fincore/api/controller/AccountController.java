cc6df5a122b72d6fc55e2aa53571b7c9429c23ff
Alexander Costa <alexander.cbss@hotmail.com>
2026-02-05 08:08:11 -0300

    criacao da accountController

--- src/main/java/br/leetjourney/fincore/api/controller/AccountController.java
+++ src/main/java/br/leetjourney/fincore/api/controller/AccountController.java
@@ -2,15 +2,14 @@ package br.leetjourney.fincore.api.controller;
 
 import br.leetjourney.fincore.api.dto.request.AccountRequest;
 import br.leetjourney.fincore.api.dto.response.AccountResponse;
-import br.leetjourney.fincore.core.security.AccountService;
+import br.leetjourney.fincore.core.service.AccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/accounts")
@@ -23,5 +22,12 @@ public class AccountController {
     public ResponseEntity<AccountResponse> create(@RequestBody @Valid AccountRequest request) {
         var accountResponse = accountService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(accountResponse);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<AccountResponse>>findAll(){
+        var accountResponse = accountService.findAll();
+        return ResponseEntity.ok(accountResponse);
     }
 }
